54fddd8618c74e33c2ef9518b0104aa185e53545
borisliao <borisliaos@gmail.com>
2025-02-17 17:13:22 -0500

    Added tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   integrations: [react()],
   site: "https://borisliao.github.io",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
