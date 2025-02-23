b054da7a05dc4b3ad53cabd1d1a2369b7f1dba09
borisliao <borisliaos@gmail.com>
2025-02-23 14:40:39 -0500

    Fixed bug with storybook image rendering

--- src/components/Film.stories.tsx
+++ src/components/Film.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Film } from "./Film";
 
+import image from "../images/IMG_7524.jpeg?url";
+
 const meta = {
   title: "Film",
 } satisfies Meta;
@@ -9,5 +11,5 @@ export default meta;
 
 export const _Film: StoryObj<typeof Film> = {
   render: (args) => <Film {...args} />,
-  args: { src: "src/images/IMG_7524.jpeg" },
+  args: { src: image },
 };
