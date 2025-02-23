1549362ee9ab5bb26ba1c83f36964786fb3f3cf7
borisliao <borisliaos@gmail.com>
2025-02-23 15:46:04 -0500

    Added Film Component #2

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -6,7 +6,10 @@ export const Film = ({
   ...divProps
 }: { src: string; alt: string } & HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div {...divProps}>
+    <div
+      {...divProps}
+      className={`${divProps.className} pt-7 pr-4 pb-16 pl-4 shadow-md`}
+    >
       <img src={src} alt={alt} />
     </div>
   );
