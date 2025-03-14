2a0d70ac7e12e59836447b80fa76c0b07e9ead0a
borisliao <borisliaos@gmail.com>
2025-03-13 21:10:07 -0400

    Updated naming of links and style

--- src/components/Film.stories.tsx
+++ src/components/Film.stories.tsx
@@ -17,6 +17,6 @@ export const _Film: StoryObj<typeof Film> = {
   args: {
     src: image,
     alt: "A apartment tower under construction in Long Island City, Jan 2025",
-    className: "w-96 bg-neutral-50 dark:bg-neutral-300",
+    className: "w-96 bg-neutral-50 dark:bg-neutral-600",
   },
 };
