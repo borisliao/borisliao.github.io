de4eee6e0702ccd19a1d159d0da99287e4c9eaa3
borisliao <borisliaos@gmail.com>
2025-03-13 21:04:04 -0400

    Added grid images to hdr page.

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -3,15 +3,20 @@ import type { HTMLAttributes } from "react";
 export const Film = ({
   src,
   alt,
+  caption,
   ...divProps
-}: { src: string; alt: string } & HTMLAttributes<HTMLDivElement>) => {
+}: {
+  src: string;
+  alt: string;
+  caption?: string;
+} & HTMLAttributes<HTMLDivElement>) => {
   return (
     <figure
       {...divProps}
-      className={`${divProps.className} pt-7 pr-4 ${alt ? "pb-4" : "pb-16"} pl-4 shadow-md`}
+      className={`${divProps.className} pt-7 pr-4 ${caption ? "pb-4" : "pb-16"} pl-4 shadow-md`}
     >
       <img src={src} alt={alt} />
-      <figcaption className="text-sm/6">{alt}</figcaption>
+      <figcaption className="text-sm/6">{caption}</figcaption>
     </figure>
   );
 };
