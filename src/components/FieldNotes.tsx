395c8126b71c808a6fdfe888b843ca1df7ddd441
borisliao <borisliaos@gmail.com>
2025-06-03 00:31:59 -0400

    Added FieldNotes component

--- src/components/FieldNotes.tsx
+++ src/components/FieldNotes.tsx
@@ -1,4 +1,20 @@
-export const FieldNotes = () => {
+import type { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
+
+export const FieldNotes = ({
+  ...props
+}: DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>) => {
   // Ratio: 1.57:1 A6
-  return <div className="h-1.5 w-1 border border-gray-300">Field Notes</div>;
+  return (
+    <a
+      {...props}
+      className="block aspect-[1/1.57] rounded-tr-lg rounded-br-lg border border-gray-300 bg-orange-200 p-10 text-gray-800 shadow-lg duration-300 hover:rotate-2 focus:rotate-2"
+    >
+      <p className="text-center font-[Futura] text-3xl font-semibold tracking-wider">
+        FIELD NOTES
+      </p>
+    </a>
+  );
 };
