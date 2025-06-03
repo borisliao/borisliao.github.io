f2d0248a4207ede4e5f99928ee017aa0975e0545
borisliao <borisliaos@gmail.com>
2025-06-03 01:01:17 -0400

    Added dark mode to FieldNotes

--- src/components/FieldNotes.tsx
+++ src/components/FieldNotes.tsx
@@ -10,11 +10,14 @@ export const FieldNotes = ({
   return (
     <a
       {...props}
-      className="block aspect-[1/1.57] rounded-tr-lg rounded-br-lg border border-gray-300
-        bg-orange-200 p-10 text-gray-800 shadow-lg duration-300 hover:rotate-2
-        focus:rotate-2"
+      className="dark:bg-linear-to-t dark:from-zinc-700 dark:to-zinc-800 block aspect-[1/1.57]
+        rounded-tr-lg rounded-br-lg border border-gray-300 bg-orange-200 p-10
+        text-gray-800 shadow-lg duration-300 hover:rotate-2 focus:rotate-2"
     >
-      <p className="text-center font-[Futura] text-3xl font-semibold tracking-wider">
+      <p
+        className="text-center font-[Futura] text-3xl font-semibold tracking-wider
+          dark:text-gray-400"
+      >
         FIELD NOTES
       </p>
     </a>
