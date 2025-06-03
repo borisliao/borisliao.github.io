80d581d624ba281dda1f6a08e5576f4a3905a93c
borisliao <borisliaos@gmail.com>
2025-06-03 00:38:26 -0400

    Added prettier-plugin-classnames

--- src/components/FieldNotes.tsx
+++ src/components/FieldNotes.tsx
@@ -10,7 +10,9 @@ export const FieldNotes = ({
   return (
     <a
       {...props}
-      className="block aspect-[1/1.57] rounded-tr-lg rounded-br-lg border border-gray-300 bg-orange-200 p-10 text-gray-800 shadow-lg duration-300 hover:rotate-2 focus:rotate-2"
+      className="block aspect-[1/1.57] rounded-tr-lg rounded-br-lg border border-gray-300
+        bg-orange-200 p-10 text-gray-800 shadow-lg duration-300 hover:rotate-2
+        focus:rotate-2"
     >
       <p className="text-center font-[Futura] text-3xl font-semibold tracking-wider">
         FIELD NOTES
